368de5b6523d498befe1bb4528f49d82baed9c3b
Anastasia <anastasiia.denisova1993@gmail.com>
2022-09-28 20:57:53 +0300

    Сделали приветствие

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Ася")
+{
+    Console.WriteLine ("Ура, это Ася!");
+
+}
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }
