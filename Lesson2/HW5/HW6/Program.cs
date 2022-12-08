3197471d180a2226110d14504dc40730fdbb3b7b
Anastasia <anastasiia.denisova1993@gmail.com>
2022-12-09 00:11:09 +0300

    New commit

--- Lesson2/HW5/HW6/Program.cs
+++ Lesson2/HW5/HW6/Program.cs
@@ -44,26 +44,26 @@
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите значение координаты K1 ");
-int k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите значение координаты B1 ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите значение координаты K2 ");
-int k2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите значение координаты B2 ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение координаты K1 ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение координаты B1 ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение координаты K2 ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение координаты B2 ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
 
-void Method4(double k1, double b1, double k2, double b2)
-{
-    if (k1 == k2 && b1 == b2)
-    { Console.WriteLine("Линии совпадают "); }
-    else if (k1 == k2)
-    { Console.WriteLine("Линии параллельны "); }
-    else
-    {
-        double x = (b2 - b1) / (k1 - k2);
-        double y = k1 * x + b1;
-        Console.WriteLine("Линии пересекаются в точке: " + x + " " + y);
-    }
-}
-Method4(k1, b1, k2, b2);
+// void Method4(double k1, double b1, double k2, double b2)
+// {
+//     if (k1 == k2 && b1 == b2)
+//     { Console.WriteLine("Линии совпадают "); }
+//     else if (k1 == k2)
+//     { Console.WriteLine("Линии параллельны "); }
+//     else
+//     {
+//         double x = (b2 - b1) / (k1 - k2);
+//         double y = k1 * x + b1;
+//         Console.WriteLine("Линии пересекаются в точке: " + x + " " + y);
+//     }
+// }
+// Method4(k1, b1, k2, b2);
---
+++ Lesson2/HW7/Program.cs
@@ -0,0 +1,154 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Console.WriteLine("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int n = ConvertToInt32(Console.ReadLine());
+// double [,] matrix = new double [m, n];
+
+// double [,] Method1(double[,] fillingArray)
+// {
+//     for (int i = 0; i < fillingArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < fillingArray.GetLength(1); j++)
+//         {
+//             fillingArray[i,j] = new Random().NextDouble()+ new Random().Next(-10, 10);
+//         }
+//     }
+//       return fillingArray;
+// }
+// void Method2(double[,] printArray)
+// {
+//        for (int i = 0; i < printArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < printArray.GetLength(1); j++)
+//         {
+//            Console.Write($"{Math.Round(printArray[i, j],1)} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Method2(Method1(matrix));
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+// Console.WriteLine("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// double [,] matrix = new double [m, n];
+
+// double [,] Method1(double[,] fillingArray)
+// {
+//     for (int i = 0; i < fillingArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < fillingArray.GetLength(1); j++)
+//         {
+//             fillingArray[i,j] = new Random().Next(1, 10);
+//         }
+//     }
+//       return fillingArray;
+// }
+
+// void Method2(double[,] printArray)
+// {
+//        for (int i = 0; i < printArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < printArray.GetLength(1); j++)
+//         {
+//            Console.Write(printArray[i, j]+ " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void Method3(double[,] printArray)
+// {
+//     int count = 0;
+//     Console.WriteLine("Введите число: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//        for (int i = 0; i < printArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < printArray.GetLength(1); j++)
+//         {
+
+//             if (printArray[i, j] == num)
+// {
+// Console.WriteLine($"{num} находится в ячейке {i},{j} ");
+// count++;
+// } 
+//         }
+//     }
+
+//        if (count == 0 ) Console.WriteLine($"Элемент отсутствует в массиве");
+// }
+// Method2(Method1(matrix));
+// Method3(matrix);
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Console.WriteLine("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// double[,] matrix = new double[m, n];
+
+// double[,] Method1(double[,] fillingArray)
+// {
+//     for (int i = 0; i < fillingArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < fillingArray.GetLength(1); j++)
+//         {
+//             fillingArray[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+//     return fillingArray;
+// }
+
+// void Method2(double[,] printArray)
+// {
+//     for (int i = 0; i < printArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < printArray.GetLength(1); j++)
+//         {
+//             Console.Write(printArray[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void Method3(double[,] printArray)
+// {
+//     double[] array = new double[printArray.GetLength(1)];
+//     {
+//         for (int i = 0; i < printArray.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < printArray.GetLength(1); j++)
+//             {
+//                 array[j] += printArray[i, j];
+//             }
+//         }
+//         for (int i = 0; i < array.Length; i++)
+//         {
+// array[i]/= array.Length;
+// Console.WriteLine("Среднее арифметическое столбца "+ (i + 1) + "= " + Math.Round(array[i],1));
+//         }
+//     }
+// }
+//     Method2(Method1(matrix));
+//     Method3(matrix);
